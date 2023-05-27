fba4ac3a51a21288a142544cb8fc02263f409c53
Nagaraj N <nagaraj.srjons@gmail.com>
2023-05-27 20:25:48 +0530

    dark-mode enabled

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Make Quiz' });
 });
 
+router.get('/create-quiz', function(req, res, next) {
+  res.render('create-quiz', { title: 'Make Quiz' });
+});
+
 module.exports = router;
